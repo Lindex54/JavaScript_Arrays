d23abb6f0411c57f232b6793ee28e84b20564d2b
Malinde Godwin <malindegodwin9@gmail.com>
2025-08-24 18:17:28 +0300

    first commit

--- script.js
+++ script.js
@@ -61,10 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -223,6 +225,13 @@ btnClose.addEventListener('click', function (e) {
   }
 
   inputCloseUsername.value = inputClosePin.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 
 // calcDisplayBalance(account1.movements);
@@ -495,12 +504,43 @@ const overlBalance2 = accounts
 
 // sort method mutates the original array
 const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
-console.log(owners.sort());
+// console.log(owners.sort());
 
 // return < 0, A, B (keep order)
 // return > 0, B, A (switch order)
-movements.sort((a, b) => {
-  if (a > b) return 1;
-  if (b > a) return -1;
-});
-console.log(movements);
+
+// Ascending
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (b > a) return -1;
+// });
+// console.log(movements);
+
+const movementsAsscendOrder = movements.sort((a, b) => a - b);
+// console.log(movementsAsscendOrder);
+
+// Descending
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (b > a) return 1;
+// });
+
+const movementsDescendingOrder = movements.sort((a, b) => b - a);
+// console.log(movementsDescendingOrder);
+
+// creating arrays progromatically
+const arr1 = [1, 2, 3, 4, 5, 6, 7];
+
+// Empty arrays + fill method
+const x = new Array(7);
+x.fill(1, 3, 5);
+// x.fill(1);
+// console.log(x);
+arr.fill(23, 2, 6);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (cur, i) => i + 1);
+console.log(z);
